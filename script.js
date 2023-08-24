26e6dc719e5f738fe839cae0df155fde6a352bee
katyaachizh <141716784+katyaachizh@users.noreply.github.com>
2023-08-24 13:55:16 +0300

    Домашнее задание 4
    Создала новые элементы списка, которые добавляются с клавиши enter, элементы можно зачеркнуть и вернуть в прежний вид.

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
+const sendInput = document.querySelector('#input');
+const itemContainer = document.querySelector('#items');
+
+sendInput.addEventListener('keydown', function(event) {
+    if (event.key == 'Enter') {
+        const itemText = sendInput.value;
+        const newItems = document.createElement('div');
+        newItems.classList.add('items');
+        newItems.textContent = itemText;
+
+        newItems.addEventListener('click', function() {
+            newItems.classList.toggle('done');
+        })
+
+        sendInput.value = " ";
+            if(itemText != " ") {
+                itemContainer.append(newItems);
+            }
+    }
+});
+
+
+
+
 /* Новые элементы должны добавляться в список по нажатию на Enter */
 
 /* Пустые элементы не должны добавляться */
